8e37ab03e621c9fcbea04363210456f84c54fe6c
rameyzaz <rameyzaalyaz@student.ub.ac.id>
2026-04-06 20:45:46 +0700

    Create MahasiswaFILKOM.java

---
+++ modul6/MahasiswaFILKOM.java
@@ -0,0 +1,57 @@
+
+
+public class MahasiswaFILKOM extends Manusia {
+    private String nim;
+    private double ipk;
+
+   public MahasiswaFILKOM(String nim, double ipk, String nama, String nik, boolean jenisKelamin, boolean menikah){
+    super(nama, jenisKelamin, nik, menikah);
+    this.nim = nim;
+    this.ipk = ipk;
+}
+
+    public String getStatus () {
+        String angkatan = "20" + nim.substring(0,2);
+        char kodeProdi = nim.charAt(6);
+        
+        String prodi= "";
+        switch(kodeProdi) {
+            case'2' : prodi = "Teknik Informatika";
+            break;
+            case'3' : prodi= "Teknik Komputer";
+            break;
+            case '4' : prodi = "Sistem Informasi";
+            break;
+            case '6' : prodi = "Pendidikan Teknologi Informasi";
+            break;
+            case '7' : prodi = "Teknologi Informasi";
+            break;
+
+            
+        }
+
+        return prodi + ", " + angkatan;
+
+    }
+
+    public double getBeasiswa () {
+        if (ipk >= 3.5) return 75;
+        else if (ipk >= 3.0) return 50;
+        else return 0;
+
+    }
+
+    public double getPendapatan() {
+        return super.getPendapatan() + getBeasiswa();
+    }
+   @Override
+    public String toString() {
+       return super.toString() + "\n" +
+        String.format(
+            "NIM           : %s\n" +
+            "IPK           : %.2f\n" +
+            "Status        : %s",
+            nim, ipk, getStatus()
+        );
+    }
+}
